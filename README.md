14ad8a5c8c728590ec396ed05b6b2d27189bffe3
MZTBTG <mztbtg@hotmail.com>
2021-11-11 17:57:15 -0300

    Atualização para versão 1.1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# backup-smb-to-windows
+# backup-smb-to-windows V1.1
 
 Faça backup de um HD Windows completo de forma automatizada, ignorando pastas inúteis e poupando espaço no seu servidor SMB (ou HD externo).
 Esse Script foi escrito para agilizar processos de backup de um HD Windows a partir do Linux. Isso pode ser muito útil quando você precisa de um backup completo, mas seu Windows não inicia, ou antes de uma formatação completa, ou ao trocar de HD, ou outros casos.
@@ -6,6 +6,15 @@ Esse Script foi escrito para agilizar processos de backup de um HD Windows a par
 ## Modo de uso
 
 Antes de tudo, configure seu servidor SMB ou seu HD extero na linha comentada no código.
+O comando "quiet" pode ser adicionado no final do comando para que sejam exibidos apenas erros na tela.
 Após configurado, digite: `sudo ./copy.sh ano-mes-dia-nomeDoComputador sdxx` (O comando `sudo` só será necessário se você não estiver como **root**)
 
 Exemplo: `./copy.sh 2021-12-31-escritorio sda2`
+         `./copy.sh 2021-12-31-escritorio sda2 quiet`
+
+# Novidades:
+
+## 1.1
+
+- Adicionado arquivo de log de erros "registro de erro" para validar a integridade dos dados após verificação de uso de espaço do disco de origem;
+- Permite mais de um backup ao mesmo tempo na mesma máquina.
--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e '==>   Backup de Windows para SMB automatizado  <==\n\n\033[05;31mCriado por MZTBTG!\033[m\n'
+echo -e '==>   Backup automatizado V1.1  <==\n\n\033[05;31mCriado MZTBTG!\033[m\n'
 
 bkp="/mnt/backup"
 
@@ -13,11 +13,16 @@ then
     mkdir $bkp
     echo "==> Criado a pasta de backup."
   fi
-  echo "==> Montando a pasta de backup."
-  mount //192.168.184.6/backup-clientes $bkp -o vers=1.0 # Edite alguns parâmetros dessa linha
+  verif=$(ls $bkp |wc -l |cut -d ' ' -f 1)
+  if [ $verif -eq 0 ]
+  then  
+    echo "==> Montando a pasta de backup."
+    mount //192.168.184.6/backup-clientes $bkp -o vers=1.0 # Edite alguns parâmetros aqui
+    echo "==> Pasta de backup montada."
+  else
+    echo "==> O backup já foi montado anteriormente."
   if [ $? -eq 0 ]
   then
-    echo "==> Pasta de backup montada."
     if [[ -d $bkp/$1 ]]
     then
       echo "==> A pasta do cliente já foi criada."
@@ -43,34 +48,34 @@ then
           cp -rnvPa !(\$Recycle.Bin|Windows|pagefile.sys|hiberfil.sys|System\ Volume\ Information) $bkp/$1
         fi
     '
-
     # ====================== Inicio do bloco de comparação ==========================
     echo -e "\n+--------------------------------------------------------------------+"
     echo -e "|                          \033[01;32mBackup concluido\033[m                          |"
     echo "|                                                                    |"
+    echo "|Para saber se a fonte dos dados está com erro, verifique o arquivo  |"
+    echo "|\"registro de erro\" localizado na pasta de backup do nosso cliente.|"
+    echo "|                                                                    |"
     echo "|Vamos iniciar a verificação dos arquivos.                           |"
     echo '|Abra um novo terminal e digite "ssh 192.168.184.6".                 |'
     echo '|Digite agora a palavra "yes" para confirmar.                        |'
-    echo '|Coloque a senha logo em seguida.                                    |'
+    echo '|Pessa a senha para um de nossos técnicos e coloque logo em seguida. |'
     echo "|                                                                    |"
     echo "|Copie o código seguinte digitando Ctrl+Shift+C e cole no terminal.  |"
     echo -e "+--------------------------------------------------------------------+\n"
     echo "cd /backup/clientes/$1;du -h --max-depth=1"
     echo -e "\n==> Compare da forma correta os resultados.\n"
-    du -h --max-depth=1
+    du -h --max-depth=1 2>$bkp/$1/registro\ de\ erro
+    error=$(wc -l $bkp/$1/registro\ de\ erro |cut -d ' ' -f 1)
+    echo -ne "\n==> A fonte de dados possui $error erro(s)."
     # ======================= Fim do bloco de comparação ===========================
-
     echo -e "\n\033[05;32mSeu backup foi concluido\033[m\n"
-    umount $bkp
     else
       echo -e '\n\033[01;31mO nome do caminho que você digitou está incorreto. Confira sua sintaxe e tente novamente.\033[m\n'
-      umount $bkp
     fi
   else
-    echo -e '\n\033[01;31mOcorreu um erro ao montar o backup.\033[m\n'
-    umount $bkp
+    echo -e '\n\033[01;31mOcorreu um erro ao montar o backup. Verifique sua conexão com internet ou os cabos.\033[m\n'
   fi
 else
   echo -e "Modo de uso:\nDigite sudo $0 ano-mes-dia-nomeDoComputador sdxx"
-  echo 'Caso você queira uma transferência de dados levemente mais rápida e limpa, digite "quiet" no final da sintaxe.'
+  echo 'Caso você queira uma transferência de dados levemente mais rápida, digite "quiet" no final da sintaxe.'
 fi
